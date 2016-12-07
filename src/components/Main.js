9b1ddaa2dd3b7a4932939060b1202d32d35aacab
jer <757303624@qq.com>
2016-12-07 16:51:17 +0800

    finish gallery-by-react

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ require('normalize.css/normalize.css');
 require('styles/App.scss');
 
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
 
 //获取图片相关的数据
@@ -28,7 +28,7 @@ function get30DegRandom(){
 	return ((Math.random() > 0.5 ? '' : '') + Math.ceil(Math.random() * 30));
 }
 
-class ImgFigure extends React.Component{
+class ImgFigureComponent extends React.Component{
 
 	//imgFigure的点击处理函数
 	handleClick = (ev) => {
@@ -43,6 +43,7 @@ class ImgFigure extends React.Component{
 	}
 
 	render(){
+
 		let styleObj = {};
 
 		//如果props属性中指定了这张图片的位置，则使用
@@ -65,8 +66,12 @@ class ImgFigure extends React.Component{
 		let imgFigureClassName = 'img-figure';
 		imgFigureClassName += this.props.arrange.isInverse ? ' is-inverse' : '';
 
+
 		return (
-			<figure className={imgFigureClassName} style={styleObj} onClick={this.handleClick} >
+			<figure className={imgFigureClassName}
+					style={styleObj}
+					onClick={this.handleClick}
+					>
 				<img src={this.props.data.imageURL}
 						alt={this.props.data.title}/>
 				<figcaption>
@@ -83,8 +88,23 @@ class ImgFigure extends React.Component{
 }
 
 //添加控制组件coomponent
-class ControllerUnit extends React.Component{
+class ControllerUnitComponent extends React.Component{
+	// 写法有问题：会提示报错cannot read property 'props' of null 在95行
+	//原因：还不知道？？？？？
+	// handleClick(ev){
+	// 	//如果点击的是当前正在选中态的图片，则翻转图片，否则将对应的图片居中
+	// 	if (this.props.arrange.isCenter) {
+	// 		this.props.inverse();
+	// 	}else{
+	// 		this.props.center();
+	// 	}
+
+	// 	ev.preventDefault();
+	// 	ev.stopPropagation();
+	// }
+
 	handleClick = (ev) => {
+		//如果点击的是当前正在选中态的图片，则翻转图片，否则将对应的图片居中
 		if (this.props.arrange.isCenter) {
 			this.props.inverse();
 		}else{
@@ -95,7 +115,6 @@ class ControllerUnit extends React.Component{
 		ev.stopPropagation();
 	}
 
-	
 	render(){
 		let controllerUnitClassName = 'controller-unit';
 		//如果对应的是居中图片，显示控制按钮的居中态
@@ -108,19 +127,65 @@ class ControllerUnit extends React.Component{
 		}
 
 		return(
-			<span className={controllerUnitClassName} onClick={this.handleClick}/>
+			<span className={controllerUnitClassName}
+			onClick={this.handleClick}
+			/>
 		);
 	}
 }
 
-class App extends React.Component {
+class AppComponent extends React.Component {
+	//因为react版本太高，要使用支持es6语法把下面属性定义到app类的构造函数中
+	// Constant:{
+	// 	centerPos:{
+	// 		left:0,
+	// 		top:0
+	// 	},
+	// 	hPosRange:{		//水平方向的取值范围(左右分区)
+	// 		leftSecX:[0,0],
+	// 		rightSecX:[0,0],
+	// 		y:[0,0]
+	// 	},
+	// 	vPosRange:{ 	//垂直方向上的取值范围(上分区)
+	// 		x:[0,0],
+	// 		topY:[0,0]
+	// 	}
+	// };
+
+	//闭包this问题
+	center(index){
+		return (() =>{this.reArrange(index)}).bind(this);
+	}
+
+	// getInitialState(){
+	// 	return {
+	// 		imgsArrangeArr:[
+	// 			{
+	// 				pos:{
+	// 					left:'0',
+	// 					top:'0'
+	// 				},
+	// 				rotate: 0,			//图片旋转
+	// 				isInverse: false	//图片翻转
+	// 				isCenter:false
+	// 			}
+	// 		]
+	// 	}
+	// }
+	//因为getInitialState不支持es6 classes
 	constructor(props){
 		super(props);
-		this.state={
+		this.state = {
 			imgsArrangeArr:[
 				{
-					isCenter:false
-				}
+	    			pos:{
+	    				left:'0',
+		    			top:'0'
+	    			},
+	    			rotate: 0,
+	    			isInverse: false,
+	    			isCenter: false
+	    		}
 			]
 		};
 		this.Constant = {
@@ -138,12 +203,26 @@ class App extends React.Component {
 				topY:[0,0]
 			}
 		};
+		// this.inverse = function (){
+		// 	return function (){
+		// 		var imgsArrangeArr = this.state.imgsArrangeArr;
+
+		// 		imgsArrangeArr[index].isInverse = !imgsArrangeArr[index].isInverse;
+				
+		// 		this.setState({
+		// 			imgsArrangeArr:imgsArrangeArr
+		// 		});
+		// 	}.bind(this)
+		// }
 	}
 
-	center(index){
-		return (() =>{this.reArrange(index)}).bind(this);
-	}
-
+	/*
+	 *翻转图片
+	 *@params index 输出当前被执行inverse操作的图片在对应的图片信息数组中的index值
+	 *@return {function} 返回一个闭包函数，其内return一个真正待被执行的函数
+	 *?????：搞清楚为什么使用闭包（图片翻转课程05；55）
+	*/
+	//闭包this问题：说return is not defined；
 	inverse(index){
 		return ((()=>{
 			var imgsArrangeArr = this.state.imgsArrangeArr;
@@ -156,6 +235,8 @@ class App extends React.Component {
 		}).bind(this));
 	}
 
+	//重新布局所有图片，
+	//@params centerIndex 指定居中排布哪个图片
 	reArrange(centerIndex){
 		var imgsArrangeArr = this.state.imgsArrangeArr,
 			Constant = this.Constant,
@@ -229,6 +310,7 @@ class App extends React.Component {
 			});
 	}
 
+	//组件加载之后，为每张图片计算其位置的范围
 	componentDidMount(){
 		//首先拿到舞台的大小
 		let stageDOM = ReactDOM.findDOMNode(this.refs.stage),
@@ -270,7 +352,7 @@ class App extends React.Component {
 
   	render() {
   		let controllerUnits = [],
-  			imgFigures=[];
+    		imgFigures=[];
 
 	    imageDatas.forEach(((value,index) => {
 	    	if(!this.state.imgsArrangeArr[index]){
@@ -285,33 +367,47 @@ class App extends React.Component {
 	    		}
     		}
     		//在遍历或者循环输出去渲染子组件的时候，key必不可少
-    		imgFigures.push(<ImgFigure key={index}
+    		imgFigures.push(<ImgFigureComponent key={index}
     									data={value}
     									ref={'imgFigure'+index}
     									arrange={this.state.imgsArrangeArr[index]}
     									inverse={this.inverse(index)}
     									center={this.center(index)}
     									/>);
-
-    		controllerUnits.push(<ControllerUnit key={index}
+    		controllerUnits.push(<ControllerUnitComponent key={index}
     									arrange={this.state.imgsArrangeArr[index]}
     									inverse={this.inverse(index)}
     									center={this.center(index)}
     									/>);
     	}).bind(this));
 
-	    return (
-	      	<section className="stage" ref="stage">
-	      		<section className="img-sec">
-		      		{imgFigures}
-	      		</section>
-	      		<nav className="controller-nav">
-	      			{controllerUnits}
-	      		</nav>
-	      	</section>
-	    );
-  	}
+  //   imageDatas.forEach(function(value,index){
+		// if(!this.state.imgsArrangeArr[index]){
+		// 	this.state.imgsArrangeArr[index] = {
+		// 		pos:{
+		// 			left:'0',
+	 //    			top:'0'
+		// 		}
+		// 	};
+		// }
+
+		// imgFigures.push(<ImgFigure data={value} ref={'imgFigure'+index} arrange={this.state.imgsArrangeArr[index]}/>)
+  //   }.bind(this));
+
+    return (
+      	<section className="stage" ref="stage">
+      		<section className="img-sec">
+      			{imgFigures}
+      		</section>
+      		<nav className="controller-nav">
+      			{controllerUnits}
+      		</nav>
+      	</section>
+    );
+  }
 }
 
-export default App;
-//{imgFigures}<ImgFigure data={imageDatas} arrange={this.state.imgsArrangeArr[0]}/>
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
